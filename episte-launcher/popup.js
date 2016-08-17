d862507450df14314ab9b34494879e700d673687
Camilo Vergara <camilovergaraa@gmail.com>
2016-08-17 10:30:13 -0400

    Interface improvements. Added [d,l] options

--- episte-launcher/popup.js
+++ episte-launcher/popup.js
@@ -1,8 +1,4 @@
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('asdf');
   document.getElementById('episte_id').focus();
   var checkPageButton = document.getElementById('checkPage');
 
@@ -10,26 +6,33 @@ document.addEventListener('DOMContentLoaded', function() {
     var win = window.open(url, '_blank');
     win.focus();
   }
+
   var open_document = function() {
     var eid = '';
     var eid = document.getElementById('episte_id').value;
-    if (true || /\w{40}/.test(eid)) {
+    if (/\w{40}/.test(eid)) {
       OpenInNewTab('http://www.epistemonikos.org/en/documents/'+eid)
-    }
+    } else if (eid == 'd') {
+      OpenInNewTab('http://www.epistemonikos.org/en/documents')
+
+    } else if (eid == 'l') {
+      chrome.tabs.getSelected(null, function(tab) {
+        var _match = tab.url.match(/documents\/(\w{40})/);
+        var current_eid = _match.length == 2 ? _match[1] : null;
+        if ( current_eid ) {
+          OpenInNewTab('http://www.epistemonikos.org/en/documents/'+current_eid+'/linker')
+        }
+
+      });
+    };
   }
-  // function _open(e) {
-  //   console.log('asdfadsf');
-  //   if (e.keyCode == 13) {
-  //     open_document();
-  //   }
-  //   return false;
-  // }
+
   checkPageButton.addEventListener('click', open_document, false);
 
   $('#episte-form').submit(function(e){
     e.preventDefault();
     open_document();
     return false; // just to be sure.
-});
+  });
 
 }, false);
